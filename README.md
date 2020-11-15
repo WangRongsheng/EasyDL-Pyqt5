b676403053aa7266c6d1b7d2eb2eb1a35619aab5
WangRongsheng <fujingshusheng@gmail.com>
2020-11-15 13:46:58 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # EasyDL-Pyqt5
 This is a GUI interface implementation of general pyqt5 called locally by easydl.
+
+# Use
+
+Please enter `app.py` code to remake it！
+
+```python
+python app.py
+```
+
---
+++ app.py
@@ -0,0 +1,188 @@
+'''
+修改31：AK，SK
+修改41：改为你的模型api
+修改112-124：模型检测输出语句与阈值调整
+'''
+
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtGui import QPixmap,QIcon
+
+from PyQt5.QtWidgets import QMenu, QPushButton, QApplication, QMainWindow, QAction, QFileDialog, QLabel, QDesktopWidget, QVBoxLayout, QGridLayout,QGroupBox,QWidget,QBoxLayout, QDockWidget
+from PyQt5.QtCore import QCoreApplication
+import sys
+import os
+import json
+import requests
+import base64
+import cv2
+
+from PyQt5 import QtWidgets
+
+def chngdir(s):
+    cfile=''
+    for i in s:
+        if i == '\\':
+            cfile += '/'
+        else:
+            cfile += i
+    return cfile
+
+def makenImgName(path):
+    fileName=path
+    # client_id 为官网获取的AK， client_secret 为官网获取的SK
+    host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=你的AK' \
+           '&client_secret=你的SK'
+    response = requests.get(host)
+    content = response.json()
+    access_token = content["access_token"]
+    imgpath = fileName
+    # .format(imgpath)
+    image = open(r'{}'.format(imgpath), 'rb').read()
+    data = {'image': base64.b64encode(image).decode()}
+    request_url = "你的模型api" + "?access_token=" + access_token
+    response = requests.post(request_url, data=json.dumps(data))
+    content = response.json()
+    content_result = content['results']
+    return content_result
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.title="EasyDL"
+        #self.top=50
+        #self.left=50
+        self.width=1200
+        self.height=800
+        self.curimg=None
+        self.curdir=None
+        self.setWindowIcon(QtGui.QIcon("logo.png"))
+        self.labelimg = QLabel(self)
+        #self.setStyleSheet("background-color: orange;")
+        self.setWindowTitle(self.title)
+        #self.setGeometry(self.top, self.left, self.width, self.height)
+        self.setFixedSize(self.width, self.height)
+        self.cadir =os.getcwd()
+        self.appLayoutCreation()
+        self.VBox = QVBoxLayout()
+        self.VBox.addWidget(self.layout)
+
+        self.wid = QWidget(self)
+        self.wid.setLayout(self.VBox)
+        self.setCentralWidget(self.wid)
+
+
+        self.create_menubar()
+        self.InitWindow()
+
+    def create_menubar(self):
+        mainmenu = self.menuBar()
+        fileMenu = mainmenu.addMenu("File")
+
+        #open image
+        openfbtn=QAction('Open Image',self)
+        openfbtn.triggered.connect(self.openf)
+        fileMenu.addAction(openfbtn)
+
+    def InitWindow(self):
+        self.showMaximized()
+
+    def openf(self):
+        options=QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open Image", "","All Files (*);;Image Files (*.png *.jpg *.jpeg)", options=options)
+        res={}
+        res=makenImgName(fileName)
+        num=0
+        for res_content in res:
+            num=num+1
+
+        if fileName:
+            self.curimg=fileName
+            self.imgfiles=None
+            self.imgtop=-1
+            self.display_img(fileName)
+
+        errorKey = 1
+        cnt=0
+
+        cimg = cv2.imread(fileName)
+
+        for res_content in res:
+            cnt=cnt+1
+            if res_content['name'] == 'nomask' and res_content['score'] < 0.6:
+                errorKey=1#佩戴不正确
+            if res_content['name'] == 'mask':
+                errorKey=0#佩戴正确
+            if errorKey == 1:
+                message = QtWidgets.QMessageBox.warning(self, "结果", "EasyDL识别结果为：图片中口罩佩戴不正确！",
+                                                        QtWidgets.QMessageBox.Cancel)
+                break
+            if errorKey == 0:
+                message = QtWidgets.QMessageBox.warning(self, "结果", "EasyDL识别结果为：图片口罩佩戴正确！",
+                                                        QtWidgets.QMessageBox.Cancel)
+                break
+
+    def openF(self):
+        dir = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        if dir:
+            self.curdir=dir
+            self.imgfiles = []
+            self.imgtop=0
+            self.curimg=None
+            for file in os.listdir(self.curdir):
+                if file.endswith(".jpeg") or file.endswith(".jpg") or file.endswith(".png") or file.endswith(".JPEG") or file.endswith(".JPG") or file.endswith(".PNG"):
+                    self.imgfiles.append(self.curdir+'/'+file)
+            self.display_img(self.imgfiles[self.imgtop])
+
+    def lftimg(self):
+        if self.curimg==None:
+            if self.imgtop==0:
+                self.imgtop=len(self.imgfiles)-1
+            else:
+                self.imgtop-=1
+            self.display_img(self.imgfiles[self.imgtop])
+    def rhtimg(self):
+        if self.curimg==None:
+            if self.imgtop==len(self.imgfiles)-1:
+                self.imgtop=0
+            else:
+                self.imgtop+=1
+            self.display_img(self.imgfiles[self.imgtop])
+
+    def appLayoutCreation(self):
+        self.leftimg=QPushButton("<",self)
+        self.leftimg.setToolTip("<h3>Left Image</h3>")
+        self.leftimg.clicked.connect(self.lftimg)
+
+        self.rightimg = QPushButton(">", self)
+        self.rightimg.setToolTip("<h3>Right Image</h3>")
+        self.rightimg.clicked.connect(self.rhtimg)
+
+
+        self.layout=QGroupBox("")
+        self.layoutbox=QGridLayout()
+
+        self.layoutbox.addWidget(self.labelimg, 0, 1, 3, 5)
+
+
+        self.layoutbox.addWidget(self.leftimg, 4, 0)
+        self.layoutbox.addWidget(self.rightimg, 4, 6)
+
+
+
+        self.layout.setLayout(self.layoutbox)
+
+
+
+    def display_img(self,filename):
+        img=QPixmap(filename)
+        img= img.scaled(480, 480, QtCore.Qt.KeepAspectRatio)
+        self.labelimg.setPixmap(img)
+        self.labelimg.setAlignment(QtCore.Qt.AlignCenter)
+
+
+
+App=QApplication(sys.argv)
+window=Window()
+sys.exit(App.exec())
